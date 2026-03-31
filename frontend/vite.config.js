7cb20db944f27815602ebeee855b38df8add3367
omi290 <omp723033@gmail.com>
2026-03-31 14:29:15 +0530

    learning page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        learn: resolve(__dirname, 'learn.html'),
+      }
+    }
   }
 })
-
